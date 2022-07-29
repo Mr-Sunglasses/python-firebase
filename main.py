4f4d5f66a05b97a00c2bb030071daae1a1badd08
Kanishk Pachauri <krishna03012003@icloud.com>
2022-07-29 19:44:38 +0530

    Learned Firebase

--- main.py
+++ main.py
@@ -1,36 +1,81 @@
+import urllib.request
+
 from api_key import API_KEY
 import pyrebase
 
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 firebase = pyrebase.initialize_app(API_KEY)
 
-auth = firebase.auth()
-# firebase.storage()
-# firebase.database()
+# auth = firebase.auth()
+# storage = firebase.storage()
+database = firebase.database()
 
 
 # Auth Email
-choice = int(input("Press 1 to Sign In, Press 2 to Register: "))
+# choice = int(input("Press 1 to Sign In, Press 2 to Register: "))
+#
+# if choice == 1:
+#     email = input("Enter Your Email: ")
+#     password = input("Enter Your Password: ")
+#
+#     try:
+#         auth.sign_in_with_email_and_password(email, password)
+#         print("Sign In Successfully")
+#     except:
+#         print("Please Enter Valid Email and Password")
+#
+# if choice == 2:
+#     email = input("Enter Your Email: ")
+#     password = input("Enter Your Password: ")
+#     confirm_password = input("Please Confirm your above Password: ")
+#
+#     if password == confirm_password:
+#         try:
+#             auth.create_user_with_email_and_password(email, password)
+#             print("User Created SuccessFully")
+#         except:
+#             print("Email Already exists")
+#     else:
+#         print("Password != Confirm_Password")
 
-if choice == 1:
-    email = input("Enter Your Email: ")
-    password = input("Enter Your Password: ")
+# Login
+# def upload():
+#     username = input("Please Enter your name")
+#     filename = input("Please Enter the File You can to Upload")
+#     cloud_file_name = f"{username}/{filename}"
+#     storage.child(cloud_file_name).put(filename)
+#
+# def view():
+#     username = input("Please Enter your name")
+#     filename = input("Please Enter the File You can to Upload")
+#     cloud_file_name = f"{username}/{filename}"
+#     print(storage.child(cloud_file_name).get_url(None))
+#
+# def download():
+#     username = input("Please Enter your name")
+#     filename = input("Please Enter the File You can to Upload")
+#     cloud_file_name = f"{username}/{filename}"
+#     storage.child(cloud_file_name).download("",filename=filename)
+#
+# def read_file():
+#     username = input("Please Enter your name")
+#     filename = input("Please Enter the File You can to Upload")
+#     cloud_file_name = f"{username}/{filename}"
+#     url = storage.child(cloud_file_name).get_url(None)
+#     f = urllib.request.urlopen(url).read()
+#     print(url)
+#
+#
+# read_file()
 
-    try:
-        auth.sign_in_with_email_and_password(email, password)
-        print("Sign In Successfully")
-    except:
-        print("Please Enter Valid Email and Password")
+data = {
+    'age': 20,
+    'address': "Banaras",
+    'friend': True,
+    'name': "Anjuman Hassan"
+}
 
-if choice == 2:
-    email = input("Enter Your Email: ")
-    password = input("Enter Your Password: ")
-    confirm_password = input("Please Confirm your above Password: ")
-
-    if password == confirm_password:
-        try:
-            auth.create_user_with_email_and_password(email, password)
-            print("User Created SuccessFully")
-        except:
-            print("Email Already exists")
-    else:
-        print("Password != Confirm_Password")
+database.push(data)
